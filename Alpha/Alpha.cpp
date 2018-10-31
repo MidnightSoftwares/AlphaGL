5477d7008acb9ba3e846e3eff1ca6bc37745b002
MidnightSoftwares <midnightsoftwares@gmail.com>
2018-10-31 17:04:06 +0100

    Simple SDL/OpenGL implemention.

--- Alpha/Alpha.cpp
+++ Alpha/Alpha.cpp
@@ -1,27 +1,82 @@
-﻿#define _DEBUG
+﻿#include "Registry.h"
 
-#include "Registry.h"
+#include <SDL2/SDL.h>
 
-#include <iostream>
-
-struct Test {
-    int x;
-};
+#include <GL/glew.h>
 
 int main(int /*argc*/, char* /*argv*/[]) {
+    // SDL init
+    const int sdlInit = SDL_Init(SDL_INIT_VIDEO);
+    ASSERT(sdlInit >= 0);
+
+    // OpenGL attribures
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    DEBUG_ONLY(SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG));
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+
+    // Window creation
+    SDL_Window* const window
+        = SDL_CreateWindow("Alpha", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720,
+                           SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+    ASSERT(window);
+
+    // Context creation
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    ASSERT(context);
+    SDL_GL_SetSwapInterval(1);
+
+    // GLEW init
+    glewExperimental = GL_TRUE;
+    GLenum glewError = glewInit();
+    ASSERT(glewError == GLEW_OK);
+
+    // OpenGL init
+    glClearColor(1.f, 0.f, 1.f, 1.f);
+
+    // Application init
     Registry registry;
 
-    int e1 = registry.create();
-    registry.attach<Test>(e1, 42);
+    // Application loop
+    bool quit{};
+    while (!quit) {
+        // SDL events
+        SDL_Event sdlEvent;
+        while (SDL_PollEvent(&sdlEvent) != 0) {
+            switch (sdlEvent.type) {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
 
-    int e2 = registry.create();
-    registry.attach<Test>(e2, 123);
+                case SDL_KEYDOWN:
+                    if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
+                        quit = true;
+                    }
+                    break;
 
-    registry.setParent(e1, e2);
+                default:
+                    break;
+            }
+        }
 
-    registry.view<Test>([](int entity, int depth) {
-        std::cout << "Entity: " << entity << " | Depth: " << depth << '\n';
-    });
+        // Fixed update
+        //
 
+        // Frame update
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //
+
+        SDL_GL_SwapWindow(window);
+    }
+
+    // Cleanup
+    SDL_GL_DeleteContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return EXIT_SUCCESS;
 }
